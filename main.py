6c65bb2d9b75610543d348ced9e6d6c42114f2d1
ForstMaple <forstmaple@gmail.com>
2021-06-08 21:17:51 +0800

    added date validation, added more information while working, and improvde variable consistency

--- main.py
+++ main.py
@@ -45,14 +45,17 @@ saved_query_path = os.path.join(cwd, '.saved_query')
 request_url = 'http://www.cninfo.com.cn/new/hisAnnouncement/query'
 max_attempts = 3
 
+base_date = datetime.strptime('2000-01-01', "%Y-%m-%d").replace(tzinfo=timezone(timedelta(hours=8)))
+
 
 class Query:
-    def __init__(self, query_name, searchkey, query_code_list,
+    def __init__(self, query_name, searchkey, query_code_list, stock_list,
                  from_date, to_date=None,
-                 stock_list=None, last_update_time=None, record_num=None, result=None):
+                 last_update_time=None, record_num=None, result=None):
         self.query_name = query_name
         self.searchkey = searchkey
         self._query_code_list = query_code_list
+        self._stock_list = stock_list
         self.from_date =from_date
         self.to_date = to_date
         self._last_update_time = last_update_time
@@ -65,7 +68,7 @@ class Query:
         status = f'''
         Query name: {self.query_name}
         Search keyword: {self.searchkey}
-        Stock list length: {len(self._stock_list)}
+        Stock list length: {len(self._query_code_list)}
         Search keyword: {self.searchkey}
         Time range: {time_range}
         Most recent update: {self._record_num} records in total on {self._last_update_time}
@@ -83,13 +86,17 @@ class Query:
         return [query_code[0:6] for query_code in self._query_code_list]
     
     @property
+    def stock_list(self):
+        return self._stock_list
+    
+    @property
     def result(self):
         return self._result
     
     def save(self):
         os.makedirs('.saved_query', exist_ok=True)
         output = os.path.join(saved_query_path, self.query_name+'.json')
-        result_json = self._result.to_json(orient='index', force_ascii=False)
+        result_json = self._result.to_json(orient='index', force_ascii=False, indent=4)
         dict_to_save = {'query_name': self.query_name, 
                         'searchkey': self.searchkey,
                         'query_code_list': self._query_code_list,
@@ -97,8 +104,8 @@ class Query:
                         'to_date': self.to_date,
                         'stock_list': self._stock_list,
                         'last_update_time': self._last_update_time, 
-                        'record_num': self._last_update_time, 
-                        'result': result_json}
+                        'record_num': self._last_update_time}
+        dict_to_save['result'] = json.loads(result_json)
         with open(output, 'w') as f:
             json.dump(dict_to_save, f, ensure_ascii=False, indent=4)
     
@@ -109,9 +116,7 @@ class Query:
         else:
             query_result_df = notice_query(self._query_code_list, self.searchkey, self.from_date, use_converter=False)
             new_record_num = query_result_df.shape[0]
-            if first_update:
-                self._stock_list = list(query_result_df['secName'].unique())
-            else:
+            if not first_update:
                 print(f'{new_record_num - self._record_num} new notice(s) since last update on {self._last_update_time}.')
             self._last_update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             self._record_num = new_record_num
@@ -121,6 +126,9 @@ class Query:
             self.save()
         else:
             pass
+        
+    def edit(target_list=None, from_date=None, to_date=None):
+        pass
         
     def download(self):
         pass
@@ -140,7 +148,12 @@ def converter():
     
     def convert(input):
         possible_stock_code = re.compile(r'\d{6}')
+        code_for_company_name = re.compile(r'\d{6},') # For retreiving company names only
         try:
+            if code_for_company_name.match(input):
+                code = input[0:6]
+                zwjc = rf_list.set_index('code').at[code, 'zwjc']
+                return zwjc
             if possible_stock_code.match(input):
                 orgId = rf_list.set_index('code').at[input, 'orgId']
                 return input + ',' + orgId
@@ -156,22 +169,33 @@ def converter():
 global_converter = converter()         
         
 def calculable_date(date=None):
+    # Maybe exception should be better designed.
+    bj = timezone(timedelta(hours=8))
     try:
         bj_date = datetime.strptime(date, '%Y-%m-%d')
+        bj_date = bj_date.replace(tzinfo=bj)
     except ValueError:
         bj_date = datetime.strptime(date, '%Y%m%d') 
+        bj_date = bj_date.replace(tzinfo=bj)
     except TypeError:
         utc = datetime.utcnow().replace(tzinfo=timezone.utc)
-        bj_date = utc.astimezone(timezone(timedelta(hours=8)))
+        bj_date = utc.astimezone(bj)
         
     def from_date(n_days=0):
-        return (bj_date + timedelta(days=n_days)).strftime('%Y-%m-%d')
+        return bj_date + timedelta(days=n_days)
     
     return from_date
 
 def format_seDate(from_date=None, to_date=None):
+
     calculable_to_date = calculable_date(to_date)
     calculable_from_date = calculable_date(from_date)
+    
+    if calculable_from_date(0) < base_date:
+        raise ValueError('cninfo has no record before 2000-01-01, please provide a later date.')
+    
+    if calculable_from_date(0) > calculable_to_date(0):
+        raise ValueError('"from_date" should be no later than "to_date".')
     
     # The to_date will be "tommorow" if not specified so as to get the latest notices,
     # since a notice is usually released on the night just before the stated date.
@@ -179,7 +203,8 @@ def format_seDate(from_date=None, to_date=None):
     
     # The from_date will be one year before the to_date if not specified.
     query_from_date = calculable_to_date(-365) if from_date is None else calculable_from_date(0)
-    return query_from_date + '~' + query_to_date
+    
+    return query_from_date.strftime('%Y-%m-%d') + '~' + query_to_date.strftime('%Y-%m-%d')
     
 def notice_query(input_list, searchkey=None, from_date=None, to_date=None, use_converter=True):
     if isinstance(input_list, str):
@@ -197,7 +222,10 @@ def notice_query(input_list, searchkey=None, from_date=None, to_date=None, use_c
     
     for query_code in query_code_list:
         temp_df = get_query_page(stock=query_code, searchkey=searchkey, seDate=seDate)
-        df = df.append(temp_df, ignore_index=True)
+        try:
+            df = df.append(temp_df, ignore_index=True)
+        except:
+            pass
     
     return df
         
@@ -226,6 +254,9 @@ def get_query_page(stock, searchkey, seDate):
                 break
     first_query_result = first_query.json()
     record_num = first_query_result['totalAnnouncement']
+    if record_num == 0:
+        print(f'[Warning] Found 0 record for {global_converter(stock[0:7])}.')
+        return None
     result_list = first_query_result['announcements']
     total_page = record_num // 30 + 1
     
@@ -244,6 +275,7 @@ def get_query_page(stock, searchkey, seDate):
     result_df = pd.DataFrame(result_list)[cols_to_preserve]
     result_df['announcementTime'] = result_df['announcementTime'].map(lambda x: date.fromtimestamp(int(str(x)[:10])))
     result_df['adjunctUrl'] = result_df['adjunctUrl'].map(lambda x: 'http://static.cninfo.com.cn/' + x)
+    print(f'Found {result_df.shape[0]} record(s) for {global_converter(stock[0:7])}.')
         
     return result_df
             
@@ -253,9 +285,24 @@ def new_query(query_name, input_list, from_date, to_date=None, searchkey=None):
         input_list = [input_list]
     else:
         pass
+    
     query_code_list = [global_converter(input) for input in input_list]
+    
+    calculable_from_date = calculable_date(from_date)
+    from_date = calculable_from_date(0).strftime('%Y-%m-%d')
+    
+    if to_date:
+        calculable_to_date = calculable_date(to_date)
+        to_date = calculable_to_date(0).strftime('%Y-%m-%d')
+    else:
+        pass
+    
+    # When a code corresponds with no records, it will not appear in the .result
+    temp_list = [query_code[0:7] for query_code in query_code_list]
+    stock_list = [global_converter(temp_code) for temp_code in temp_list]
+    
     query = Query(query_name=query_name, searchkey=searchkey, 
-                  query_code_list=query_code_list, from_date=from_date, to_date=to_date)
+                  query_code_list=query_code_list, from_date=from_date, to_date=to_date, stock_list=stock_list)
     print(f'Query "{query_name}" has been created.\nUpdating...')
     query.update(first_update=True)
     query.status
